2da0f6c53b571cdbc6d4535560f2f13078361d5b
gavirusada <153696230+gavirusada@users.noreply.github.com>
2024-12-02 07:30:50 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: 'QUEENELISA;;;==QUvpGU11EOKRULqRkasNGcBVXZxQjWmBlcYFla0VnaTdTN2QTdsdTZ1dXdjEkSUFVekNWVpwd=83412520749',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
